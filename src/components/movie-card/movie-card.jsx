8cad1e9a2787b7395616e63e071e610f893d98cf
Adrian <ad.dever@yahoo.com>
2023-11-07 20:24:25 +0200

    updated movie cards

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,26 +1,30 @@
 import PropTypes from "prop-types";
+import { Button, Card } from 'react-bootstrap'
+
+import "../movie-card/movie-card.scss"
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <div
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
-            {movie.title}
-        </div>
+        <Card className="movie-card">
+            <Card.Img variant="top" src={movie.image} className="movie-image" />
+            <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{movie.director}</Card.Text>
+                <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+            </Card.Body>
+        </Card>
     );
 };
 
 // Define PropTypes for the MovieCard component
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        id: PropTypes.number.isRequired,
+        // id: PropTypes.number.isRequired,
         title: PropTypes.string.isRequired,
-        poster_path: PropTypes.string.isRequired,
-        overview: PropTypes.string.isRequired,
-        genre_ids: PropTypes.arrayOf(PropTypes.number).isRequired,
-        release_date: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        // genre_ids: PropTypes.arrayOf(PropTypes.number).isRequired,
+        // release_date: PropTypes.string.isRequired,
     }).isRequired,
     onMovieClick: PropTypes.func.isRequired,
 };
